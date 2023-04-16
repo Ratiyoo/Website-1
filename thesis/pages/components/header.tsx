c4fea11c15b330aa2c89f384e1c361ef58961555
Ratiyoo <81957254+Ratiyoo@users.noreply.github.com>
2023-04-16 02:08:40 -0700

    update

--- thesis/pages/components/header.tsx
+++ thesis/pages/components/header.tsx
@@ -1,1 +1,13 @@
 import styles from "@/styles/header.module.css";
+
+export default function Header() {
+  return (
+    <>
+      <div className={styles.header_wrapper}>
+        <div>Test</div>
+        <div>Test</div>
+        <div>Test</div>
+      </div>
+    </>
+  );
+}
