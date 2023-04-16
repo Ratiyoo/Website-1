9a884da6bdc38e271bbe42dd031827515391835d
Ratiyoo <81957254+Ratiyoo@users.noreply.github.com>
2023-04-16 04:05:03 -0700

    Skeleton

--- thesis/pages/components/header.tsx
+++ thesis/pages/components/header.tsx
@@ -3,10 +3,12 @@ import styles from "@/styles/header.module.css";
 export default function Header() {
   return (
     <>
+      <div className={styles.title}>Title Text</div>
       <div className={styles.header_wrapper}>
-        <div>Test</div>
-        <div>Test</div>
-        <div>Test</div>
+        <div className={styles.synopsis}>
+          <div>image</div>
+          <div>Test</div>
+        </div>
       </div>
     </>
   );
