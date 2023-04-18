2ebe0cbba1d295f0aca6b8cc923525af1eef54ff
Ratiyoo <heraccle@gmail.com>
2023-04-18 23:42:14 +0400

    Update
    Added some(soon to be buttons), a picture, fixed alignments, and fixed display problem.

--- thesis/pages/components/header.tsx
+++ thesis/pages/components/header.tsx
@@ -1,14 +1,27 @@
 import styles from "@/styles/header.module.css";
+import Rati from "@/public/images/myPic.jpg";
+import Image from 'next/image'
 
-export default function Header() {
+export default function Header(props: any) {
   return (
     <>
-      <div className={styles.title}>Title Text</div>
+      
       <div className={styles.header_wrapper}>
-        <div className={styles.synopsis}>
-          <div>image</div>
-          <div>Test</div>
+
+      <div className={styles.title}>Title Text</div>
+
+        <div className={styles.autobiogrophy}>
+          <div ><Image src={Rati} alt="IDK"  className={styles.picture}/></div>
+          <div className={styles.bio}>bio</div>
         </div>
+
+        <div className={styles.buttons}>
+          <a className={styles.dropdown}>My Experience</a>
+          <a className={styles.dropdown}>My Accomplishments</a>
+          <a className={styles.dropdown}>Personal Life
+          <div className={styles.dropdown_content}> Random facts</div></a>
+        </div>
+
       </div>
     </>
   );
