11e18e68ae6d4b3ee93555c2874c9673a7d95d48
Ratiyoo <heraccle@gmail.com>
2023-04-22 21:33:19 +0400

    Button Changes
    Made the hover only work on buttons and not the whole div, made the contact buttons functional but not styled yet

--- thesis/pages/components/header.tsx
+++ thesis/pages/components/header.tsx
@@ -8,20 +8,22 @@ export default function Header(props: any) {
       
       <div className={styles.header_wrapper}>
 
-      <div className={styles.title}>Title Text</div>
+        <div className={styles.title}>Title Text</div>
 
         <div className={styles.autobiogrophy}>
           <div ><Image src={Rati} alt="IDK"  className={styles.picture}/></div>
           <div className={styles.bio}>bio</div>
         </div>
 
-        <div className={styles.buttons}>
-          <a className={styles.dropdown}>My Experience</a>
-          <a className={styles.dropdown}>My Accomplishments</a>
-          <a className={styles.dropdown}>Personal Life
-          <div className={styles.dropdown_content}> Random facts</div></a>
+        <div className={styles.button_wrapper}>
+          <a className={styles.buttons}>My Experience</a>
+          <a className={styles.buttons}>My Accomplishments</a>
+          <a className={styles.buttons}>Personal Life</a>
         </div>
-
+        <div className={styles.contact_wrapper}>
+          <a href="https://www.linkedin.com/in/rati-kereselidze-729969250/" className={styles.contacts}> LinkedIn</a>
+          <a href= "https://github.com/Ratiyoo" className={styles.contacts}> Github</a>
+        </div>
       </div>
     </>
   );
