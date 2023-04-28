072118a8a34c62ff48d6a1150151d0b13954e4c7
Ratiyoo <heraccle@gmail.com>
2023-04-28 12:25:08 +0400

    design commit

--- thesis/pages/components/header.tsx
+++ thesis/pages/components/header.tsx
@@ -1,6 +1,8 @@
 import styles from "@/styles/header.module.css";
 import Rati from "@/public/images/myPic.jpg";
 import Image from 'next/image'
+import Github from "@/public/images/GitHub.png";
+import Linkedin from "@/public/images/linkedin.png";
 
 export default function Header(props: any) {
   return (
@@ -11,7 +13,7 @@ export default function Header(props: any) {
         <div className={styles.title}>My Portfolio</div>
 
         <div className={styles.autobiogrophy}>
-          <div ><Image src={Rati} alt="IDK"  className={styles.picture}/></div>
+          <div ><Image src={Rati} alt="MyImage"  className={styles.picture}/></div>
           <div className={styles.bio}>bio</div>
         </div>
 
@@ -21,10 +23,10 @@ export default function Header(props: any) {
           <a className={styles.buttons}>Personal Life</a>
         </div>
         <div className={styles.contact_wrapper}>
-          <div>Contact me on:</div>
-          <a href="https://www.linkedin.com/in/rati-kereselidze-729969250/" className={styles.contacts}> LinkedIn</a>
-          <a href= "https://github.com/Ratiyoo" className={styles.contacts}> Github</a>
-          <div> or email me at: rati.kereselidze20@gmail.com</div>
+          <div className={styles.contact_text}>Contact me on:</div>
+          <a href="https://www.linkedin.com/in/rati-kereselidze-729969250/" className={styles.contacts}><Image src={Linkedin} alt="LinkedinLogo"  className={styles.contact_image}/> LinkedIn</a>
+          <a href= "https://github.com/Ratiyoo" className={styles.contacts}><Image src={Github} alt="GithubLogo"  className={styles.contact_image}/> Github</a>
+          <div className={styles.contact_text}> or email me at: rati.kereselidze20@gmail.com</div>
         </div>
       </div>
     </>
