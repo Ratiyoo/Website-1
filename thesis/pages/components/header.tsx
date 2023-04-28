27bd1266cdb722a945883e5806aa906f7e0fbf6e
Ratiyoo <81957254+Ratiyoo@users.noreply.github.com>
2023-04-28 07:44:30 -0700

    dropdown commit

--- thesis/pages/components/header.tsx
+++ thesis/pages/components/header.tsx
@@ -1,32 +1,70 @@
 import styles from "@/styles/header.module.css";
 import Rati from "@/public/images/myPic.jpg";
-import Image from 'next/image'
+import Image from "next/image";
 import Github from "@/public/images/GitHub.png";
 import Linkedin from "@/public/images/linkedin.png";
 
 export default function Header(props: any) {
   return (
     <>
-      
       <div className={styles.header_wrapper}>
-
         <div className={styles.title}>My Portfolio</div>
 
         <div className={styles.autobiogrophy}>
-          <div ><Image src={Rati} alt="MyImage"  className={styles.picture}/></div>
+          <div>
+            <Image src={Rati} alt="MyImage" className={styles.picture} />
+          </div>
           <div className={styles.bio}>bio</div>
         </div>
 
         <div className={styles.button_wrapper}>
-          <a className={styles.buttons}>My Experience</a>
-          <a className={styles.buttons}>My Accomplishments</a>
-          <a className={styles.buttons}>Personal Life</a>
+          <select name="dropdown" id="dropdown" className={styles.buttons}>
+            <option>My Experience</option>
+            <option>info</option>
+            <option>info</option>
+            <option>
+              <a>Click to see more</a>
+            </option>
+          </select>
+
+          <select className={styles.buttons}>
+            <option>My Skillset</option>
+            <option>info</option>
+            <option>info</option>
+            <option>info</option>
+          </select>
+          <select className={styles.buttons}>
+            <option>Personal Life</option>
+            <option>info</option>
+            <option>info</option>
+            <option>info</option>
+          </select>
         </div>
         <div className={styles.contact_wrapper}>
           <div className={styles.contact_text}>Contact me on:</div>
-          <a href="https://www.linkedin.com/in/rati-kereselidze-729969250/" className={styles.contacts}><Image src={Linkedin} alt="LinkedinLogo"  className={styles.contact_image}/> LinkedIn</a>
-          <a href= "https://github.com/Ratiyoo" className={styles.contacts}><Image src={Github} alt="GithubLogo"  className={styles.contact_image}/> Github</a>
-          <div className={styles.contact_text}> or email me at: rati.kereselidze20@gmail.com</div>
+          <a
+            href="https://www.linkedin.com/in/rati-kereselidze-729969250/"
+            className={styles.contacts}
+          >
+            <Image
+              src={Linkedin}
+              alt="LinkedinLogo"
+              className={styles.contact_image}
+            />{" "}
+            LinkedIn
+          </a>
+          <a href="https://github.com/Ratiyoo" className={styles.contacts}>
+            <Image
+              src={Github}
+              alt="GithubLogo"
+              className={styles.contact_image}
+            />{" "}
+            Github
+          </a>
+          <div className={styles.contact_text}>
+            {" "}
+            or email me at: rati.kereselidze20@gmail.com
+          </div>
         </div>
       </div>
     </>
